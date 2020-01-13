d03857136082726f06936c53184017ec3219769b
Sergio Mesa <media@qbo3d.com>
2020-01-12 22:18:03 -0500

    Initial Commit

---
+++ app/src/main/java/com/eltiempo/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.eltiempo.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
